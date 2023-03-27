d9ac19a6ac59709689dc197b3b451ab22614aab4
Tommy <tommy@gmail.com>
2023-03-27 10:30:29 -0700

    eventListener practice

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttonRef = document.querySelector("button")
+
+function alertUser() {
+    alert("You clicked!!!")
+    // buttonRef.removeEventListener("click", alertUser)
+}
+
+buttonRef.addEventListener("click", alertUser, {once:true})
+
+
