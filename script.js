f139778e4f1aa0183ca4823d952f0aded904b6c1
Tommy <tommy@gmail.com>
2023-03-27 10:46:42 -0700

    button practice

--- script.js
+++ script.js
@@ -7,4 +7,19 @@ function alertUser() {
 
 buttonRef.addEventListener("click", alertUser, {once:true})
 
+function changeBGPink(){
+    // document.body.style.backgroundColor = "pink";
+    document.body.classList.add("pinkBG")
+}
+buttonRef.addEventListener("click", changeBGPink)
+
+function changeText(){   
+    if (buttonRef.textContent === "Click Me!"){
+        buttonRef.textContent = "Clicked!"
+    }else{
+        buttonRef.textContent = "Click Me!"
+    }
+}
+buttonRef.addEventListener("click", changeText)
+
 
